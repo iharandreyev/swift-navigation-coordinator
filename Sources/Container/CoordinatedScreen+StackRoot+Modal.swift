dbedf5b13f66193a686013a5d34666ca559d3a27
Ihar Andreyeu <iandreyeu@scnsoft.com>
2025-03-26 02:08:31 +0400

    Introduce modal stack root container for coordinators that manage stack root and can present modals

---
+++ Sources/Container/CoordinatedScreen+StackRoot+Modal.swift
@@ -0,0 +1,44 @@
+//
+//  CoordinatedScreen+StackRoot+Modal.swift
+//  SUINavigationCoordinator
+//
+//  Created by Andreyeu, Ihar on 3/26/25.
+//
+
+import SwiftUI
+
+public extension CoordinatedScreen {
+  /// Creates a container view that interfaces with a stack coordinator.
+  /// Use this factory method for cases when the managing coordinator interfaces with both `StackNavigator` and `ModalNavigator`.
+  ///
+  /// The view configures infrastructure to:
+  /// * pass `build root view` requrest to the coordinator;
+  /// * pass `build destination view` requrests to the coordinator;
+  /// * observe destinations pushed into coordinator's `StackNavigator`;
+  /// * observe destinations to be presented using coordinator's `ModalNavigator`;
+  /// * send `coordinator.finish` when the view is removed from view hierarchy;
+  static func stackRoot<CoordinatorType: ScreenCoordinatorType & StackCoordinatorType & ModalCoordinatorType>(
+    modalCoordinator coordinator: CoordinatorType
+  ) -> some View {
+    CoordinatedScreen_StackRoot_Modal(coordinator: coordinator)
+  }
+}
+
+public struct CoordinatedScreen_StackRoot_Modal<CoordinatorType: ScreenCoordinatorType & StackCoordinatorType & ModalCoordinatorType>: View {
+  private let coordinator: CoordinatorType
+  
+  init(
+    coordinator: CoordinatorType
+  ) {
+    self.coordinator = coordinator
+  }
+  
+  public var body: some View {
+    CoordinatedScreen_StackRoot(
+      coordinator: coordinator
+    )
+    .modal(
+      for: coordinator
+    )
+  }
+}
