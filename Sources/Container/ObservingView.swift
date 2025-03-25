0a1c5c61b56beffe3f54668f859737a05ad8089f
Ihar Andreyeu <iandreyeu@scnsoft.com>
2025-03-26 01:47:28 +0400

    Implement `ObservingView` container to encapsulate `WithPerceptionTracking` invocations

---
+++ Sources/Container/ObservingView.swift
@@ -0,0 +1,35 @@
+//
+//  ObservingView.swift
+//  SUINavigationCoordinator
+//
+//  Created by Andreyeu, Ihar on 3/26/25.
+//
+
+import SwiftUI
+import Perception
+
+@available(
+  iOS, deprecated: 17, message: "'ObservingView' is no longer needed in iOS 17+"
+)
+@available(
+  macOS, deprecated: 14, message: "'ObservingView' is no longer needed in macOS 14+"
+)
+@available(
+  watchOS, deprecated: 10, message: "'ObservingView' is no longer needed in watchOS 10+"
+)
+@available(
+  tvOS, deprecated: 17, message: "'ObservingView' is no longer needed in tvOS 17+"
+)
+public protocol ObservingView<Content>: View where Body == WithPerceptionTracking<Content> {
+  associatedtype Content: View
+  
+  var content: Content { get }
+}
+
+extension ObservingView {
+  public var body: Body {
+    WithPerceptionTracking {
+      content
+    }
+  }
+}
