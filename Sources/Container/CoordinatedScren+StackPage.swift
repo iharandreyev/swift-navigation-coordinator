09271ecf1503d8814cdd0723da1cd06953f1e018
Ihar Andreyeu <iandreyeu@scnsoft.com>
2025-03-26 01:55:19 +0400

    Introduce stack page container for coordinators that manage a screen inside a stack

---
+++ Sources/Container/CoordinatedScren+StackPage.swift
@@ -0,0 +1,43 @@
+//
+//  CoordinatedScren+StackPage.swift
+//  SUINavigationCoordinator
+//
+//  Created by Andreyeu, Ihar on 3/26/25.
+//
+
+import SwiftUI
+
+public extension CoordinatedScreen {
+  /// Creates a container view that interfaces with a stack coordinator, that is responsible for managing one of the steps in a flow, that is presented as a stack.
+  /// Use this factory method for cases when the managing coordinator interfaces only with `StackNavigator`.
+  ///
+  /// The view configures infrastructure to:
+  /// * pass `build step root view` requrests to the coordinator;
+  /// * pass `build destination view` requrests to the coordinator;
+  /// * observe destinations pushed into coordinator's `StackNavigator`;
+  /// * send `coordinator.finish` when the view is removed from view hierarchy;
+  static func stackPage<CoordinatorType: ScreenCoordinatorType & StackCoordinatorType>(
+    stackCoordinator coordinator: CoordinatorType
+  ) -> some View {
+    CoordinatedScreen_StackPage(coordinator: coordinator)
+  }
+}
+
+public struct CoordinatedScreen_StackPage<CoordinatorType: ScreenCoordinatorType & StackCoordinatorType>: View {
+  private let coordinator: CoordinatorType
+
+  init(
+    coordinator: CoordinatorType
+  ) {
+    self.coordinator = coordinator
+  }
+  
+  public var body: some View {
+    CoordinatedScreen_Base(
+      coordinator: coordinator
+    )
+    .navigationDestination(
+      for: coordinator
+    )
+  }
+}
