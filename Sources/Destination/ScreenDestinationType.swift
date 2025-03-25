a2431152a96308c38751851fe8eece35e0da8bdc
Ihar Andreyeu <iandreyeu@scnsoft.com>
2025-03-26 01:19:19 +0400

    Implement stack navigator as a wrapper for stackstate to be interfaced with coordinator

---
+++ Sources/Destination/ScreenDestinationType.swift
@@ -0,0 +1,8 @@
+//
+//  ScreenDestinationType.swift
+//  SUINavigationCoordinator
+//
+//  Created by Andreyeu, Ihar on 3/26/25.
+//
+
+public protocol ScreenDestinationType: Sendable, Hashable { }
---
+++ Sources/Navigator/StackNavigator.swift
@@ -0,0 +1,70 @@
+//
+//  StackNavigator.swift
+//  SUINavigationCoordinator
+//
+//  Created by Andreyeu, Ihar on 3/26/25.
+//
+
+@MainActor
+public protocol StackNavigatorType<DestinationType> {
+  associatedtype DestinationType: ScreenDestinationType
+  
+  var state: StackState { get }
+  
+  func push(_ destination: DestinationType)
+  
+  func replaceLast(with destination: DestinationType)
+  func replaceStack(with destination: DestinationType)
+  
+  func pop()
+  func popToRoot()
+  
+  func scope<
+    ChildDestinationType: ScreenDestinationType
+  >() -> any StackNavigatorType<ChildDestinationType>
+}
+
+@MainActor
+public final class StackNavigator<
+  DestinationType: ScreenDestinationType
+>: StackNavigatorType {
+  public let state: StackState
+
+  public init(
+    state: StackState = StackState()
+  ) {
+    self.state = state
+  }
+  
+  public func push(_ destination: DestinationType) {
+    state.append(destination)
+  }
+  
+  public func replaceLast(with destination: DestinationType) {
+    #warning("TODO: Figure out how to replace stack with animation")
+    pop()
+    push(destination)
+  }
+  
+  public func replaceStack(with destination: DestinationType) {
+    #warning("TODO: Figure out how to replace stack with animation")
+    popToRoot()
+    push(destination)
+  }
+  
+  public func pop() {
+    state.removeLast()
+  }
+  
+  public func popToRoot() {
+    state.removeLast(state.count)
+  }
+  
+  public func scope<
+    ChildDestinationType: ScreenDestinationType
+  >() -> any StackNavigatorType<ChildDestinationType> {
+    StackNavigator<ChildDestinationType>(
+      state: state
+    )
+  }
+}
