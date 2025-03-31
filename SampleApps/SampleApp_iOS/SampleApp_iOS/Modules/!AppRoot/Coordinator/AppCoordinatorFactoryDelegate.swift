5163183e4df88074868ebada97f3e1f5f8f7f36f
Ihar Andreyeu <iandreyeu@scnsoft.com>
2025-04-01 01:04:13 +0400

    Show app init screen on app start

--- SampleApps/SampleApp_iOS/SampleApp_iOS/Modules/!AppRoot/Coordinator/AppCoordinatorFactoryDelegate.swift
+++ SampleApps/SampleApp_iOS/SampleApp_iOS/Modules/!AppRoot/Coordinator/AppCoordinatorFactoryDelegate.swift
@@ -23,3 +23,83 @@ protocol AppCoordinatorFactoryDelegateType: CoordinatorFactoryDelegateType {
   
   func createMainCoordinator() -> MainCoordinatorType
 }
+
+struct AppCoordinatorFactoryDelegate: AppCoordinatorFactoryDelegateType {
+  func createAppInitScreen(
+    onFinish: @escaping () -> Void
+  ) -> some View {
+    AppInitScreen(onFinish: onFinish)
+  }
+  
+  func createOnboardingCoordinator(
+    onFinish: @escaping () -> Void
+  ) -> some ScreenCoordinatorType & StackCoordinatorType & ModalCoordinatorType {
+    OnboardingCoordinator(
+      stackNavigator: StackNavigator(),
+      modalNavigator: ModalNavigator(),
+      onFinish: onFinish
+    )
+  }
+  
+  func createMainCoordinator() -> some StaticSpecimenCoordinatorType & LabelledSpecimenCoordinatorType {
+    MainCoordinator(
+      specimenNavigator: SpecimenNavigator(initialDestination: MainCoordinator.DestinationType())
+    )
+  }
+}
+
+private final class OnboardingCoordinator: CoordinatorBase, ScreenCoordinatorType, StackCoordinatorType, ModalCoordinatorType {
+  struct DestinationType: ScreenDestinationType, ModalDestinationContentType {
+    let id = "static-id"
+  }
+  
+  let stackNavigator: StackNavigator<DestinationType>
+  let modalNavigator: ModalNavigator<DestinationType>
+  
+  init(
+    stackNavigator: StackNavigator<DestinationType>,
+    modalNavigator: ModalNavigator<DestinationType>,
+    onFinish: @escaping () -> Void
+  ) {
+    self.stackNavigator = stackNavigator
+    self.modalNavigator = modalNavigator
+    
+    super.init(onFinish: onFinish)
+  }
+
+  func initialScreen() -> some View {
+    Text("TBD")
+  }
+  
+  func screen(for destination: DestinationType) -> some View {
+    Text("TBD")
+  }
+}
+
+private final class MainCoordinator: CoordinatorBase, StaticSpecimenCoordinatorType, LabelledSpecimenCoordinatorType {
+  struct DestinationType: ScreenDestinationType, CaseIterable {
+    let id = "static-id"
+    
+    static let allCases: [MainCoordinator.DestinationType] = [DestinationType()]
+  }
+  
+  let specimenNavigator: SpecimenNavigator<DestinationType>
+  
+  init(
+    specimenNavigator: SpecimenNavigator<DestinationType>
+  ) {
+    self.specimenNavigator = specimenNavigator
+    
+    super.init()
+  }
+
+  @ViewBuilder
+  func screenContent(for destination: DestinationType) -> some View {
+    Text("TBD")
+  }
+  
+  @ViewBuilder
+  func label(for tab: DestinationType) -> some View {
+    Text("TBD")
+  }
+}
