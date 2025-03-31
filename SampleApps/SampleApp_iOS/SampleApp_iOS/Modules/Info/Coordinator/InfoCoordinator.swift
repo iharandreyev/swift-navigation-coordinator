722fbc5bbfcf3ae728e519567c3d8b6ff21f2daa
Ihar Andreyeu <iandreyeu@scnsoft.com>
2025-04-01 01:45:19 +0400

    Implement info coordinator

---
+++ SampleApps/SampleApp_iOS/SampleApp_iOS/Modules/Info/Coordinator/InfoCoordinator.swift
@@ -0,0 +1,57 @@
+//
+//  InfoCoordinator.swift
+//  SampleApp_iOS
+//
+//  Created by Andreyeu, Ihar on 4/1/25.
+//
+
+import SwiftNavigationCoordinator
+import SwiftUI
+
+enum InfoDestination: String, ScreenDestinationType {
+  case last
+}
+
+@MainActor
+final class InfoCoordinator<
+  FactoryDelegateType: InfoCoordinatorFactoryDelegateType
+>: CoordinatorBase, CoordinatorType, ScreenCoordinatorType, StackCoordinatorType {
+  typealias DestinationType = InfoDestination
+  
+  let stackNavigator: StackNavigator<DestinationType>
+  let factory: FactoryDelegateType
+
+  init(
+    stackNavigator: StackNavigator<DestinationType>,
+    factory: FactoryDelegateType,
+    onFinish: @escaping () -> Void
+  ) {
+    self.stackNavigator = stackNavigator
+    self.factory = factory
+    
+    super.init(onFinish: onFinish)
+  }
+  
+  func initialScreen() -> some View {
+    factory.createFirstScreen(
+      onContinue: { [weak self] in
+        self?.showLastScreen()
+      }
+    )
+  }
+
+  func screen(for destination: DestinationType) -> some View {
+    switch destination {
+    case .last:
+      factory.createLastScreen(
+        onDone: { [weak self] in
+          self?.finish()
+        }
+      )
+    }
+  }
+
+  private func showLastScreen() {
+    stackNavigator.push(.last)
+  }
+}
---
+++ SampleApps/SampleApp_iOS/SampleApp_iOS/Modules/Info/Coordinator/InfoCoordinatorFactoryDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  InfoCoordinatorFactoryDelegate.swift
+//  SampleApp_iOS
+//
+//  Created by Andreyeu, Ihar on 4/1/25.
+//
+
+import SwiftNavigationCoordinator
+import SwiftUI
+
+protocol InfoCoordinatorFactoryDelegateType: CoordinatorFactoryDelegateType {
+  associatedtype FirstScreenType: View
+  associatedtype LastScreenType: View
+  
+  func createFirstScreen(
+    onContinue: @escaping () -> Void
+  ) -> FirstScreenType
+  
+  func createLastScreen(
+    onDone: @escaping () -> Void
+  ) -> LastScreenType
+}
+
+struct InfoCoordinatorFactoryDelegate: InfoCoordinatorFactoryDelegateType {
+  func createFirstScreen(
+    onContinue: @escaping () -> Void
+  ) -> some View {
+    Text("TBD")
+  }
+  
+  func createLastScreen(
+    onDone: @escaping () -> Void
+  ) -> some View {
+    Text("TBD")
+  }
+}
