c7f2763dc7873634f88d3f4b5c82da808c4f31c9
Ihar Andreyeu <iandreyeu@scnsoft.com>
2025-03-31 22:48:13 +0400

    Fix build errors

--- Sources/SwiftNavigationCoordinator/Container/CoordinatedScreen+Specimen.swift
+++ Sources/SwiftNavigationCoordinator/Container/CoordinatedScreen+Specimen.swift
@@ -5,25 +5,23 @@
 //  Created by Andreyeu, Ihar on 3/26/25.
 //
 
-import Perception
-import SUIOnRemoveFromParent
 import SwiftUI
+import SUIOnRemoveFromParent
 
-extension CoordinatedScreen {
+#warning("TODO: Extend support to WatchOS, macOS and TvOS")
+@available(iOS 16, *)
+public extension CoordinatedScreen {
   /// Creates a container view that interfaces with a specimen coordinator.
   /// Use this factory method for cases when the managing coordinator interfaces `SpecimenNavigator`.
   ///
   /// The view configures infrastructure to:
   /// * observe destinations to be presented using coordinator's `SpecimenNavigator`;
   /// * send `coordinator.finish` when the view is removed from view hierarchy;
-  public static func specimen<
-    CoordinatorType: SpecimenCoordinatorType,
-    Content: View
-  >(
+  static func specimen<CoordinatorType: SpecimenCoordinatorType, Content: View>(
     coordinator: CoordinatorType,
     @ViewBuilder content: @escaping (Binding<CoordinatorType.DestinationType>) -> Content
   ) -> some View {
-    _CoordinatedScreen_Specimen(
+    CoordinatedScreen_Specimen(
       coordinator: coordinator,
       content: content
     )
@@ -36,12 +34,10 @@ extension CoordinatedScreen {
   /// * pass `build destination view` requrests to the coordinator;
   /// * observe destinations to be presented using coordinator's `SpecimenNavigator`;
   /// * send `coordinator.finish` when the view is removed from view hierarchy;
-  public static func specimen<
-    CoordinatorType: SpecimenCoordinatorType
-  >(
+  static func specimen<CoordinatorType: SpecimenCoordinatorType>(
     coordinator: CoordinatorType
   ) -> some View {
-    _CoordinatedScreen_Specimen(
+    CoordinatedScreen_Specimen(
       coordinator: coordinator,
       content: { [unowned coordinator] destination in
         coordinator.screen(
@@ -52,13 +48,14 @@ extension CoordinatedScreen {
   }
 }
 
-struct _CoordinatedScreen_Specimen<
+#warning("TODO: Extend support to WatchOS, macOS and TvOS")
+@available(iOS 16, *)
+public struct CoordinatedScreen_Specimen<
   CoordinatorType: SpecimenCoordinatorType,
   Content: View
->: ObservingView {
+>: View {
   private let coordinator: CoordinatorType
-  @Perception.Bindable
-  private var state: SpecimenState
+  private let specimenNavigator: SpecimenNavigator<CoordinatorType.DestinationType>
   
   private let contentBuilder: (Binding<CoordinatorType.DestinationType>) -> Content
   
@@ -67,22 +64,19 @@ struct _CoordinatedScreen_Specimen<
     @ViewBuilder content: @escaping (Binding<CoordinatorType.DestinationType>) -> Content
   ) {
     self.coordinator = coordinator
-    self.state = coordinator.specimenNavigator.state
+    self.specimenNavigator = coordinator.specimenNavigator
     self.contentBuilder = content
   }
   
-  var content: some View {
-    contentBuilder(
-      $state.destinationOf(CoordinatorType.DestinationType.self)
+  public var body: some View {
+    SpecimenContainer(
+      specimenNavigator: coordinator.specimenNavigator,
+      destinationContent: contentBuilder
     )
     .onRemoveFromParent(
       perform: { [weak coordinator] in
         coordinator?.finish()
       }
-    )
-    .animation(
-      .easeInOut,
-      value: state._destination
     )
   }
 }
--- Sources/SwiftNavigationCoordinator/Container/CoordinatedScreen+Tabbed.swift
+++ Sources/SwiftNavigationCoordinator/Container/CoordinatedScreen+Tabbed.swift
@@ -5,11 +5,12 @@
 //  Created by Andreyeu, Ihar on 3/26/25.
 //
 
-import Perception
 import SwiftUI
 import SUIOnRemoveFromParent
 
-extension CoordinatedScreen {
+#warning("TODO: Extend support to WatchOS, macOS and TvOS")
+@available(iOS 16, *)
+public extension CoordinatedScreen {
   /// Creates a container view that interfaces with a specimen coordinator.
   /// Use this factory method for cases when the managing coordinator interfaces `SpecimenNavigator` and is intended to present `TabView`.
   ///
@@ -18,9 +19,7 @@ extension CoordinatedScreen {
   /// * pass `build tab label` requrests to the coordinator;
   /// * observe destinations to be presented using coordinator's `SpecimenNavigator`;
   /// * send `coordinator.finish` when the view is removed from view hierarchy;
-  public static func tabbed<
-    CoordinatorType: LabelledSpecimenCoordinatorType
-  >(
+  static func tabbed<CoordinatorType: LabelledSpecimenCoordinatorType>(
     coordinator: CoordinatorType
   ) -> some View where CoordinatorType.DestinationType: CaseIterable, CoordinatorType.DestinationType.AllCases: RandomAccessCollection {
     CoordinatedScreen_Tabbed(coordinator: coordinator)
@@ -34,9 +33,7 @@ extension CoordinatedScreen {
   /// * pass `build tab label` requrests to the coordinator;
   /// * observe destinations to be presented using coordinator's `SpecimenNavigator`;
   /// * send `coordinator.finish` when the view is removed from view hierarchy;
-  static func tabbed<
-    CoordinatorType: LabelledSpecimenCoordinatorType
-  >(
+  static func tabbed<CoordinatorType: LabelledSpecimenCoordinatorType>(
     coordinator: CoordinatorType,
     tabs: [CoordinatorType.DestinationType]
   ) -> some View {
@@ -44,24 +41,23 @@ extension CoordinatedScreen {
   }
 }
 
-import Perception
-import SUIOnRemoveFromParent
-
-struct CoordinatedScreen_Tabbed<
+#warning("TODO: Extend support to WatchOS, macOS and TvOS")
+@available(iOS 16, *)
+public struct CoordinatedScreen_Tabbed<
   CoordinatorType: LabelledSpecimenCoordinatorType
->: ObservingView {
+>: View {
   private let coordinator: CoordinatorType
-  @Perception.Bindable
-  private var state: SpecimenState
+  private let specimenNavigator: SpecimenNavigator<CoordinatorType.DestinationType>
   
   private let tabs: [CoordinatorType.DestinationType]
 
   init(
     coordinator: CoordinatorType
   ) where CoordinatorType.DestinationType: CaseIterable, CoordinatorType.DestinationType.AllCases: RandomAccessCollection {
-    self.coordinator = coordinator
-    self.state = coordinator.specimenNavigator.state
-    self.tabs = Array(CoordinatorType.DestinationType.allCases)
+    self.init(
+      coordinator: coordinator,
+      tabs: Array(CoordinatorType.DestinationType.allCases)
+    )
   }
   
   init(
@@ -69,36 +65,37 @@ struct CoordinatedScreen_Tabbed<
     tabs: [CoordinatorType.DestinationType]
   ) {
     self.coordinator = coordinator
-    self.state = coordinator.specimenNavigator.state
+    self.specimenNavigator = coordinator.specimenNavigator
     self.tabs = tabs
   }
   
-  var content: some View {
-    TabView(
-      selection: $state.destinationOf(CoordinatorType.DestinationType.self)
-    ) {
-      ForEach(
-        tabs,
-        id: \.self,
-        content: { [unowned coordinator] tab in
-          coordinator.screen(
-            for: tab
+  public var body: some View {
+    SpecimenContainer(
+      specimenNavigator: specimenNavigator,
+      destinationContent: { destination in
+        TabView(
+          selection: destination
+        ) {
+          ForEach(
+            tabs,
+            id: \.self,
+            content: { [unowned coordinator] tab in
+              coordinator.screen(
+                for: tab
+              )
+              .tabItem {
+                coordinator.label(for: tab)
+              }
+              .tag(tab)
+            }
           )
-          .tabItem {
-            coordinator.label(for: tab)
-          }
-          .tag(tab)
         }
-      )
-    }
+      }
+    )
     .onRemoveFromParent(
       perform: { [weak coordinator] in
         coordinator?.finish()
       }
-    )
-    .animation(
-      .easeInOut,
-      value: state._destination
     )
   }
 }
