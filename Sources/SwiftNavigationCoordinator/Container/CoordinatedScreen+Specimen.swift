c99b3c2bc16d66e3476743ac2ea6c96890f81a0a
Ihar Andreyeu <iandreyeu@scnsoft.com>
2025-03-31 23:53:52 +0400

    Refactor to reduce code duplication

--- Sources/SwiftNavigationCoordinator/Container/CoordinatedScreen+Specimen.swift
+++ Sources/SwiftNavigationCoordinator/Container/CoordinatedScreen+Specimen.swift
@@ -12,66 +12,20 @@ extension CoordinatedScreen {
   /// Use this factory method for cases when the managing coordinator interfaces `SpecimenNavigator`.
   ///
   /// The view configures infrastructure to:
-  /// * observe destinations to be presented using coordinator's `SpecimenNavigator`;
-  /// * send `coordinator.finish` when the view is removed from view hierarchy;
-  public static func specimen<
-    CoordinatorType: SpecimenCoordinatorType,
-    Content: View
-  >(
-    coordinator: CoordinatorType,
-    @ViewBuilder content: @escaping (Binding<CoordinatorType.DestinationType>) -> Content
-  ) -> some View {
-    _CoordinatedScreen_Specimen(
-      coordinator: coordinator,
-      content: content
-    )
-  }
-  
-  /// Creates a container view that interfaces with a specimen coordinator.
-  /// Use this factory method for cases when the managing coordinator interfaces `SpecimenNavigator`.
-  ///
-  /// The view configures infrastructure to:
   /// * pass `build destination view` requrests to the coordinator;
   /// * observe destinations to be presented using coordinator's `SpecimenNavigator`;
-  /// * send `coordinator.finish` when the view is removed from view hierarchy;
   public static func specimen<
     CoordinatorType: SpecimenCoordinatorType
   >(
     coordinator: CoordinatorType
   ) -> some View {
-    _CoordinatedScreen_Specimen(
-      coordinator: coordinator,
-      content: { [unowned coordinator] destination in
+    SpecimenContainer(
+      specimenNavigator: coordinator.specimenNavigator,
+      destinationContent: { [unowned coordinator] destination in
         coordinator.screen(
           for: destination.wrappedValue
         )
       }
-    )
-  }
-}
-
-struct _CoordinatedScreen_Specimen<
-  CoordinatorType: SpecimenCoordinatorType,
-  Content: View
->: View {
-  private let coordinator: CoordinatorType
-  private let specimenNavigator: SpecimenNavigator<CoordinatorType.DestinationType>
-  
-  private let contentBuilder: (Binding<CoordinatorType.DestinationType>) -> Content
-  
-  init(
-    coordinator: CoordinatorType,
-    @ViewBuilder content: @escaping (Binding<CoordinatorType.DestinationType>) -> Content
-  ) {
-    self.coordinator = coordinator
-    self.specimenNavigator = coordinator.specimenNavigator
-    self.contentBuilder = content
-  }
-  
-  var body: some View {
-    SpecimenContainer(
-      specimenNavigator: coordinator.specimenNavigator,
-      destinationContent: contentBuilder
     )
   }
 }
