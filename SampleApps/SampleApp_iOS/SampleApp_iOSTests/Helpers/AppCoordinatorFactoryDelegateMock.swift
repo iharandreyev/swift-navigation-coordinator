da01c0d92d116381fac5cc2e2590ee6da12af211
Ihar Andreyeu <iandreyeu@scnsoft.com>
2025-04-01 04:18:50 +0400

    Extend app coordinator factory delegate mock with convenience init

--- SampleApps/SampleApp_iOS/SampleApp_iOSTests/Helpers/AppCoordinatorFactoryDelegateMock.swift
+++ SampleApps/SampleApp_iOS/SampleApp_iOSTests/Helpers/AppCoordinatorFactoryDelegateMock.swift
@@ -5,9 +5,19 @@
 //  Created by Andreyeu, Ihar on 4/1/25.
 //
 
+import SwiftNavigationCoordinator
+import SwiftNavigationCoordinatorTesting
+import SwiftUI
+
+@testable
+import SampleApp_iOS
+
 #warning("TODO: Implement macro for this")
 @MainActor
-final class AppCoordinatorFactoryDelegateMock: AppCoordinatorFactoryDelegateType {
+final class AppCoordinatorFactoryDelegateMock<
+  OnboardingCoordinatorType: ScreenCoordinatorType & StackCoordinatorType & ModalCoordinatorType,
+  MainCoordinatorType: StaticSpecimenCoordinatorType & LabelledSpecimenCoordinatorType
+>: AppCoordinatorFactoryDelegateType {
   struct Params_createAppInitScreen {
     let onFinish: () -> Void
   }
@@ -39,6 +49,7 @@ final class AppCoordinatorFactoryDelegateMock: AppCoordinatorFactoryDelegateType
     let onFinish: () -> Void
   }
   
+  private let injected_createOnboardingCoordinator: (Params_createOnboardingCoordinator) -> OnboardingCoordinatorType
   private(set) var createOnboardingCoordinator_invocations: [Params_createOnboardingCoordinator] = []
   var createOnboardingCoordinator_count: Int { createOnboardingCoordinator_invocations.count }
   private var createOnboardingCoordinator_onFinish: (() -> Void)!
@@ -47,7 +58,7 @@ final class AppCoordinatorFactoryDelegateMock: AppCoordinatorFactoryDelegateType
   
   func createOnboardingCoordinator(
     onFinish: @escaping () -> Void
-  ) -> DummyCoordinator {
+  ) -> OnboardingCoordinatorType {
     let params = Params_createOnboardingCoordinator(
       onFinish: onFinish
     )
@@ -56,7 +67,7 @@ final class AppCoordinatorFactoryDelegateMock: AppCoordinatorFactoryDelegateType
     createOnboardingCoordinator_invocations.append(params)
     createOnboardingCoordinator_invoked?(params)
     
-    return DummyCoordinator()
+    return injected_createOnboardingCoordinator(params)
   }
   
   func simulate_createOnboardingCoordinator_onFinish() {
@@ -65,26 +76,62 @@ final class AppCoordinatorFactoryDelegateMock: AppCoordinatorFactoryDelegateType
   
   struct Params_createMainCoordinator { }
   
+  private let injected_createMainCoordinator: (Params_createMainCoordinator) -> MainCoordinatorType
   private(set) var createMainCoordinator_invocations: [Params_createMainCoordinator] = []
   var createMainCoordinator_count: Int { createMainCoordinator_invocations.count }
   private let createMainCoordinator_invoked: ((Params_createMainCoordinator) -> Void)?
 
-  func createMainCoordinator() -> DummyCoordinator {
+  func createMainCoordinator() -> MainCoordinatorType {
     let params = Params_createMainCoordinator()
     
     createMainCoordinator_invocations.append(params)
     createMainCoordinator_invoked?(params)
     
-    return DummyCoordinator()
+    return injected_createMainCoordinator(params)
   }
   
   init(
     createAppInitScreen_invoked: ((Params_createAppInitScreen) -> Void)? = nil,
+    createOnboardingCoordinator: @escaping (Params_createOnboardingCoordinator) -> OnboardingCoordinatorType,
     createOnboardingCoordinator_invoked: ((Params_createOnboardingCoordinator) -> Void)? = nil,
+    createMainCoordinator: @escaping (Params_createMainCoordinator) -> MainCoordinatorType,
     createMainCoordinator_invoked: ((Params_createMainCoordinator) -> Void)? = nil
   ) {
     self.createAppInitScreen_invoked = createAppInitScreen_invoked
+    self.injected_createOnboardingCoordinator = createOnboardingCoordinator
     self.createOnboardingCoordinator_invoked = createOnboardingCoordinator_invoked
+    self.injected_createMainCoordinator = createMainCoordinator
     self.createMainCoordinator_invoked = createMainCoordinator_invoked
+  }
+}
+
+extension AppCoordinatorFactoryDelegateMock {
+  convenience init(
+    createAppInitScreen_invoked: ((Params_createAppInitScreen) -> Void)? = nil,
+    createOnboardingCoordinator_invoked: ((Params_createOnboardingCoordinator) -> Void)? = nil,
+    createMainCoordinator_invoked: ((Params_createMainCoordinator) -> Void)? = nil
+  ) where OnboardingCoordinatorType == DummyCoordinator, MainCoordinatorType == DummyCoordinator {
+    self.init(
+      createAppInitScreen_invoked: createAppInitScreen_invoked,
+      createOnboardingCoordinator: { _ in DummyCoordinator() },
+      createOnboardingCoordinator_invoked: createOnboardingCoordinator_invoked,
+      createMainCoordinator: { _ in DummyCoordinator() },
+      createMainCoordinator_invoked: createMainCoordinator_invoked
+    )
+  }
+  
+  convenience init(
+    createAppInitScreen_invoked: ((Params_createAppInitScreen) -> Void)? = nil,
+    createOnboardingCoordinator_invoked: ((Params_createOnboardingCoordinator) -> Void)? = nil,
+    createMainCoordinator: @escaping (Params_createMainCoordinator) -> MainCoordinatorType,
+    createMainCoordinator_invoked: ((Params_createMainCoordinator) -> Void)? = nil
+  ) where OnboardingCoordinatorType == DummyCoordinator {
+    self.init(
+      createAppInitScreen_invoked: createAppInitScreen_invoked,
+      createOnboardingCoordinator: { _ in DummyCoordinator() },
+      createOnboardingCoordinator_invoked: createOnboardingCoordinator_invoked,
+      createMainCoordinator: createMainCoordinator,
+      createMainCoordinator_invoked: createMainCoordinator_invoked
+    )
   }
 }
