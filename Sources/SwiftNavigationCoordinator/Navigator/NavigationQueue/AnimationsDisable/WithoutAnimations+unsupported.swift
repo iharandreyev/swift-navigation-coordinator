9b90269115d2d86c751a9444f992247aaaaec33a
Ihar Andreyeu <iandreyeu@scnsoft.com>
2025-04-03 23:05:46 +0400

    Introduce animations disable for unsupported platforms

---
+++ Sources/SwiftNavigationCoordinator/Navigator/NavigationQueue/AnimationsDisable/WithoutAnimations+unsupported.swift
@@ -0,0 +1,26 @@
+//
+//  WithoutAnimations+unsupported.swift
+//  swift-navigation-coordinator
+//
+//  Created by Andreyeu, Ihar on 4/3/25.
+//
+
+#if !os(iOS)
+
+actor WithoutAnimations_unsupported {
+  let clock: AnyClock<Duration>
+  
+  init(
+    clock: AnyClock<Duration>
+  ) {
+    self.clock = clock
+  }
+  
+  func run(
+    job: @MainActor @escaping () -> Void
+  ) async {
+    fatalError("Current platform is not yet supported!")
+  }
+}
+
+#endif
