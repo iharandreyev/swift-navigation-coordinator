5c1bdf2d116147f6a7c6f2d5ca4752f0aa85c082
Ihar Andreyeu <iandreyeu@scnsoft.com>
2025-04-04 05:03:36 +0400

    Refactor deeplink coordinator testing with AutoMockable

--- SampleApps/SampleApp_iOS/SampleApp_iOSTests/Helpers/MainCoordinatorFactoryDelegateMock.swift
+++ SampleApps/SampleApp_iOS/SampleApp_iOSTests/Helpers/MainCoordinatorFactoryDelegateMock.swift
@@ -7,76 +7,63 @@
 
 import SwiftNavigationCoordinator
 import SwiftNavigationCoordinatorTesting
-import SwiftUI
 
-@testable
-import SampleApp_iOS
-
-#warning("TODO: Implement macro for this")
-@MainActor
-final class MainCoordinatorFactoryDelegateMock<
-  UsecasesCoordinatorType: ScreenCoordinatorType & StackCoordinatorType & ModalCoordinatorType,
-  DeeplinksCoordinatorType: ScreenCoordinatorType
->: MainCoordinatorFactoryDelegateType {
-  struct Params_createUsecasesCoordinator { }
+enum MainCoordinatorFactoryDelegateMock {
+  typealias Dummy = MainCoordinatorFactoryDelegateTypeMock<DummyCoordinator, DummyCoordinator>
+  typealias DummyDeeplinks<
+    UsecasesCoordinatorType: ScreenCoordinatorType & StackCoordinatorType & ModalCoordinatorType
+  > = MainCoordinatorFactoryDelegateTypeMock<DummyCoordinator, UsecasesCoordinatorType>
+  typealias DummyUsecases<
+    DeeplinksCoordinatorType: ScreenCoordinatorType
+  > = MainCoordinatorFactoryDelegateTypeMock<DeeplinksCoordinatorType, DummyCoordinator>
+  typealias Full<
+    DeeplinksCoordinatorType: ScreenCoordinatorType,
+    UsecasesCoordinatorType: ScreenCoordinatorType & StackCoordinatorType & ModalCoordinatorType
+  > = MainCoordinatorFactoryDelegateTypeMock<DeeplinksCoordinatorType, UsecasesCoordinatorType>
   
-  private let injected_ccreateUsecasesCoordinator: (Params_createUsecasesCoordinator) -> UsecasesCoordinatorType
-  private(set) var createUsecasesCoordinator_invocations: [Params_createUsecasesCoordinator] = []
-  var createUsecasesCoordinator_count: Int { createUsecasesCoordinator_invocations.count }
-  private let createUsecasesCoordinator_invoked: ((Params_createUsecasesCoordinator) -> Void)?
-  private var createUsecasesCoordinator_onFinish: Callback<Void>!
-  
-  func createUsecasesCoordinator() -> UsecasesCoordinatorType {
-    let params = Params_createUsecasesCoordinator()
-    
-    createUsecasesCoordinator_invocations.append(params)
-    createUsecasesCoordinator_invoked?(params)
-    
-    return injected_ccreateUsecasesCoordinator(params)
+  @MainActor
+  static func create() -> Dummy {
+    let factory = Dummy()
+    factory.createDeeplinksCoordinatorReturnValue = DummyCoordinator()
+    factory.createUsecasesCoordinatorReturnValue = DummyCoordinator()
+    return factory
   }
   
-  struct Params_createDeeplinksCoordinator { }
-  
-  private let injected_createDeeplinksCoordinator: (Params_createDeeplinksCoordinator) -> DeeplinksCoordinatorType
-  private(set) var createDeeplinksCoordinator_invocations: [Params_createDeeplinksCoordinator] = []
-  var createDeeplinksCoordinator_count: Int { createDeeplinksCoordinator_invocations.count }
-  private let createDeeplinksCoordinator_invoked: ((Params_createDeeplinksCoordinator) -> Void)?
-  private var createDeeplinksCoordinator_onFinish: Callback<Void>!
-  
-  func createDeeplinksCoordinator() -> DeeplinksCoordinatorType {
-    let params = Params_createDeeplinksCoordinator()
-    
-    createDeeplinksCoordinator_invocations.append(params)
-    createDeeplinksCoordinator_invoked?(params)
-    
-    return injected_createDeeplinksCoordinator(params)
+  @MainActor
+  static func create<
+    UsecasesCoordinatorType: ScreenCoordinatorType & StackCoordinatorType & ModalCoordinatorType
+  >(
+    usecasesCoordinator: UsecasesCoordinatorType
+  ) -> DummyDeeplinks<UsecasesCoordinatorType> {
+    let factory = DummyDeeplinks<UsecasesCoordinatorType>()
+    factory.createDeeplinksCoordinatorReturnValue = DummyCoordinator()
+    factory.createUsecasesCoordinatorReturnValue = usecasesCoordinator
+    return factory
   }
   
-  init(
-    createUsecasesCoordinator: @escaping (Params_createUsecasesCoordinator) -> UsecasesCoordinatorType,
-    createUsecasesCoordinator_invoked: ((Params_createUsecasesCoordinator) -> Void)? = nil,
-    createDeeplinksCoordinator: @escaping (Params_createDeeplinksCoordinator) -> DeeplinksCoordinatorType,
-    createDeeplinksCoordinator_invoked: ((Params_createDeeplinksCoordinator) -> Void)? = nil
-  ) {
-    self.injected_ccreateUsecasesCoordinator = createUsecasesCoordinator
-    self.createUsecasesCoordinator_invoked = createUsecasesCoordinator_invoked
-    self.injected_createDeeplinksCoordinator = createDeeplinksCoordinator
-    self.createDeeplinksCoordinator_invoked = createDeeplinksCoordinator_invoked
+  @MainActor
+  static func create<
+    DeeplinksCoordinatorType: ScreenCoordinatorType
+  >(
+    deeplinksCoordinator: DeeplinksCoordinatorType
+  ) -> DummyUsecases<DeeplinksCoordinatorType> {
+    let factory = DummyUsecases<DeeplinksCoordinatorType>()
+    factory.createDeeplinksCoordinatorReturnValue = deeplinksCoordinator
+    factory.createUsecasesCoordinatorReturnValue = DummyCoordinator()
+    return factory
   }
-}
-
-extension MainCoordinatorFactoryDelegateMock {
-  convenience init(
-    createUsecasesCoordinator_invoked: ((Params_createUsecasesCoordinator) -> Void)? = nil,
-    createDeeplinksCoordinator_invoked: ((Params_createDeeplinksCoordinator) -> Void)? = nil
-  ) where UsecasesCoordinatorType == DummyCoordinator, DeeplinksCoordinatorType == DummyCoordinator {
-    self.init(
-      createUsecasesCoordinator: { _ in DummyCoordinator() },
-      createUsecasesCoordinator_invoked: createUsecasesCoordinator_invoked,
-      createDeeplinksCoordinator: { _ in DummyCoordinator() },
-      createDeeplinksCoordinator_invoked: createDeeplinksCoordinator_invoked
-    )
+  
+  @MainActor
+  static func create<
+    DeeplinksCoordinatorType: ScreenCoordinatorType,
+    UsecasesCoordinatorType: ScreenCoordinatorType & StackCoordinatorType & ModalCoordinatorType
+  >(
+    deeplinksCoordinator: DeeplinksCoordinatorType,
+    usecasesCoordinator: UsecasesCoordinatorType
+  ) -> Full<DeeplinksCoordinatorType, UsecasesCoordinatorType> {
+    let factory = Full<DeeplinksCoordinatorType, UsecasesCoordinatorType>()
+    factory.createDeeplinksCoordinatorReturnValue = deeplinksCoordinator
+    factory.createUsecasesCoordinatorReturnValue = usecasesCoordinator
+    return factory
   }
 }
-
-typealias MainCoordinatorDelegateMockDummy = MainCoordinatorFactoryDelegateMock<DummyCoordinator, DummyCoordinator>
