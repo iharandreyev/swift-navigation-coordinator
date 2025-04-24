7bf163ded881e7ddac15bb2e45f4adcb7713a91b
Ihar Andreyeu <iandreyeu@scnsoft.com>
2025-04-24 22:53:58 +0400

    Fix typo

--- Sources/SwiftNavigationCoordinator/SwiftNavigationCoordinator.swift
+++ Sources/SwiftNavigationCoordinator/SwiftNavigationCoordinator.swift
@@ -9,7 +9,6 @@ public func setLogger(_ logger: LoggerType) {
   Logger.setLogger(logger)
 }
 
-@MainActor
 public func setEnvironment(_ newValue: Environment) {
   Environment.setEnvironment(newValue)
 }
