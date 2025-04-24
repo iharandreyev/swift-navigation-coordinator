ff075588026a4b3b175384c1a4baecba0f452dc8
Ihar Andreyeu <iandreyeu@scnsoft.com>
2025-04-24 23:00:12 +0400

    Remove unused code

--- Sources/SwiftNavigationCoordinator/Extensions/View+Coordinator.swift
+++ Sources/SwiftNavigationCoordinator/Extensions/View+Coordinator.swift
@@ -32,20 +32,4 @@ extension View {
       }
     )
   }
-  
-  @inline(__always)
-  public func onRemoveFromHierarchy(
-    finish coordinator: CoordinatorBase,
-    sourceFile: StaticString = #file,
-    line: UInt = #line
-  ) -> some View {
-    self.onRemoveFromParent(
-      perform: { [weak coordinator] in
-        guard let coordinator else { return }
-        guard !coordinator.isFinished else { return }
-
-        coordinator.finish(sourceFile: sourceFile, line: line)
-      }
-    )
-  }
 }
